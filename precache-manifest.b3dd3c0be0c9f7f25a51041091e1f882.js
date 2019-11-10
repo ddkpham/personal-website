935aa39379cf3f76b84d21f55e51d2b7d1067980
David Pham <dpa35@sfu.ca>
2019-11-09 21:17:07 -0800

    Updates

---
+++ precache-manifest.b3dd3c0be0c9f7f25a51041091e1f882.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "55a0dc16ea749db2f9bf2f497133f495",
+    "url": "www.ddkpham.com/personal-website/index.html"
+  },
+  {
+    "revision": "f782ab330e3d25c1adcd",
+    "url": "www.ddkpham.com/personal-website/static/css/main.2c3cdb14.chunk.css"
+  },
+  {
+    "revision": "57a10e69f1defeb4ac81",
+    "url": "www.ddkpham.com/personal-website/static/js/2.0cdc5008.chunk.js"
+  },
+  {
+    "revision": "f782ab330e3d25c1adcd",
+    "url": "www.ddkpham.com/personal-website/static/js/main.0f16df85.chunk.js"
+  },
+  {
+    "revision": "f05e16247b235f183851",
+    "url": "www.ddkpham.com/personal-website/static/js/runtime-main.ea585491.js"
+  },
+  {
+    "revision": "8cb3927f3c7d0dbffdd85ae3bf1beb10",
+    "url": "www.ddkpham.com/personal-website/static/media/Logo.8cb3927f.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "www.ddkpham.com/precache-manifest.f09f939fe96d9775dd9c29e3bf6d3f5b.js"
+  "www.ddkpham.com/personal-website/precache-manifest.b3dd3c0be0c9f7f25a51041091e1f882.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("www.ddkpham.com/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("www.ddkpham.com/personal-website/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
